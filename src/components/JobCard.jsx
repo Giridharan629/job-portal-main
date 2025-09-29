d21675d732d1c7e2433b994f910471e9eeb1d71e
Giridharan C <118421351+Giridharan629@users.noreply.github.com>
2025-09-29 15:34:04 +0530

    card design aligned

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -111,38 +111,44 @@ const JobCard = ({ job }) => {
 
   return (
     <div className="job-card">
-      <div className="job-card-header">
-        <div className="company-logo-container">{getCompanyLogo(job.logo)}</div>
-        <div className="posted-time">{job.postedTime}</div>
-      </div>
-
-      <h3 className="job-title">{job.title}</h3>
-
-      <div className="job-details">
-        <div className="detail-item">
-          
-          <img src="assets/images/exeperience.png" alt="" />
-          <span>{job.experience}</span>
-        </div>
-
-        <div className="detail-item">
-          <img src="assets/images/onsite.png" alt="" />
-          <span>{job.location}</span>
-        </div>
-
-        <div className="detail-item">
-          <img src="assets/images/salary.png" alt="" />
-          <span>{job.salary}{job.currency} /M</span>
-        </div>
-      </div>
-
-      <div className="job-description">
-        {job.description.map((desc, index) => (
-          <div key={index} className="description-item">
-            <span className="bullet">•</span>
-            <span>{desc}</span>
+      <div>
+        <div className="job-card-header">
+          <div className="company-logo-container">
+            {getCompanyLogo(job.logo)}
           </div>
-        ))}
+          <div className="posted-time">{job.postedTime}</div>
+        </div>
+
+        <h3 className="job-title">{job.title}</h3>
+
+        <div className="job-details">
+          <div className="detail-item">
+            <img src="assets/images/exeperience.png" alt="" />
+            <span>{job.experience}</span>
+          </div>
+
+          <div className="detail-item">
+            <img src="assets/images/onsite.png" alt="" />
+            <span>{job.location}</span>
+          </div>
+
+          <div className="detail-item">
+            <img src="assets/images/salary.png" alt="" />
+            <span>
+              {job.salary}
+              {job.currency} /M
+            </span>
+          </div>
+        </div>
+
+        <div className="job-description">
+          {job.description.map((desc, index) => (
+            <div key={index} className="description-item">
+              <span className="bullet">•</span>
+              <span>{desc}</span>
+            </div>
+          ))}
+        </div>
       </div>
 
       <button className="apply-btn">Apply Now</button>
